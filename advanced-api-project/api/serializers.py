d9740d96a68c64078e8fd6f72c2cff0b597099ae
ayuba <yusufayuba.a11@gmail.com>
2025-09-24 12:36:07 +0100

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,23 +1,23 @@
-from .models import Book, Author
+from datetime import date
 from rest_framework import serializers
-import datetime
-
-
-class AuthorSerializer(serializers.ModelSerializer):
-    """Serializes Author model"""
-    class Meta:
-        model = Author
-        fields = ["name"]
+from .models import Author, Book
 
 class BookSerializer(serializers.ModelSerializer):
     """Serializes Book model while nesting AuthorSerializer to display author's name"""
-    author = AuthorSerializer(many=True, read_only=True)
     class Meta:
         model = Book
-        fields = "__all__"
-        
-    def validate(self, data):
-        if data["publication_year"] > datetime.date.year:
-            raise serializers.ValidationError("Publication Year should not be in the future.")
-        return data
-        
+        fields = ['id', 'title', 'publication_year', 'author']
+
+    def validate_publication_year(self, value):
+        if value > date.today().year:
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    """Serializes Author model"""
+    # Use related_name='books' on the FK; otherwise use 'book_set'
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']

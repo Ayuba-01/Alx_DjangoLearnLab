7936687be75d58b7010f243cea36cde5d92df884
ayuba <yusufayuba.a11@gmail.com>
2025-10-02 17:44:17 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 User = get_user_model()
 

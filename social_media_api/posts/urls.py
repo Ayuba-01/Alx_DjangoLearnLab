143f2b986f81a741fd76d5e862fe32c10bec102b
ayuba <yusufayuba.a11@gmail.com>
2025-10-09 15:56:30 +0100

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -5,8 +5,12 @@ from .views import PostViewSet, CommentViewSet, FeedView
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="posts")
 router.register(r"comments", CommentViewSet, basename="comments")
+post_like_view = PostViewSet.as_view({"post": "like"})
+post_unlike_view = PostViewSet.as_view({"post": "unlike"})
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("feed/", FeedView.as_view(), name="feed")
+    path("feed/", FeedView.as_view(), name="feed"),
+    path("posts/<int:pk>/like/", post_like_view, name="post-like"),
+    path("posts/<int:pk>/unlike/", post_unlike_view, name="post-unlike"),
 ]
